2494d68ec630214c953e8b4ddc715d45504fdd19
André Ramos do Vale <andre_ramosdovale@outlook.com>
2025-11-30 18:53:53 -0300

    feat: implement radix themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Theme>
+          {children}
+          <ThemePanel />
+        </Theme>
       </body>
     </html>
   );
